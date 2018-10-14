c3013943f5aa0fc8291dec7520e573896a219ba3
Pito Salas <pitosalas@gmail.com>
2018-10-13 22:56:42 -0400

    Checkpoint work to git

--- ha.rb
+++ ha.rb
@@ -1,39 +1,48 @@
 #!/usr/bin/env ruby
+
 require 'thor'
 require 'yaml'
 require 'byebug'
 
-class Ha < Thor
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
+class Hue < Thor
+  desc "list", "list Hue items"
+  def list
+    puts "listing hue"
   end
 
   desc "pair", "run pairing process to allow you to access the hue bridge"
   def pair
     puts "Pairing... Run over to the bridge, and press the button"
   end
+end
+  
+
+CONFIG_FILE_SPEC = "#{Dir.home}/.ha"
+class Ha < Thor
+  desc "hello NAME", "say hello to NAME"
+  def hello(name)
+    puts "Hello #{name}"
+  end
+
+  desc "hue SUBCOMMAND", "commands for working with Philips Hue"
+  subcommand "hue", Hue
 
   def self.setup
-    @config_file_spec = "#{Dir.home}/.ha"
     puts "setting up environment"
-    if File.exists? @config_file_spec
-      myfile = File.open(@config_file_spec, "r")
-      @config = YAML.load(myfile.read)
-    else
-      @config = {}
+    if File.exists? CONFIG_FILE_SPEC
+      myfile = File.open(CONFIG_FILE_SPEC, "r")
+      @config = YAML.load(myfile.read) 
     end
+    @config ||= {}
   end
 
   def self.save
     puts "saving state"
-    byebug
-    @config[:name] = "Pito"
-    myfile = File.open("~/.ha", "w")
-    myfile.write(@config.to_json)
+    @config[:name] += "Salas"
+    myfile = File.open(CONFIG_FILE_SPEC, "w")
+    myfile.write(@config.to_yaml)
   end
 end
-
 
 Ha.setup
 Ha.start(ARGV)
